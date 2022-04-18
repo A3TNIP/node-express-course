27e5fef47e2782893eddfb3db85f19aa14ef93a7
A3TNIP <maharjanaaju@gmail.com>
2022-04-18 13:25:24 +0545

    Post request added

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 
+// Use Body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
 
 app.listen(8000, function() {
     console.log('listening on port 8000');
@@ -29,4 +34,22 @@ app.get('/users/:id', function(req, res) {
         message: 'successfully got user',
         user: req.params.id
     })
+})
+
+app.post('/login', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUserName = 'bob';
+    const mockPassword = '123';
+
+
+    if (username === mockUserName && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'successfully logged in'
+        })
+    } else {
+        res.json({ success: false, message: 'incorrect username or password' })
+    }
 })
