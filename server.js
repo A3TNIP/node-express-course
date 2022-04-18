1150cdeb43e1ec2eada403e01da7fed76e2f2dc0
A3TNIP <maharjanaaju@gmail.com>
2022-04-18 13:17:29 +0545

    add first GET route

--- server.js
+++ server.js
@@ -6,3 +6,17 @@ const app = express();
 app.listen(8000, function() {
     console.log('listening on port 8000');
 })
+
+
+const mockUserData = [
+    { name: 'bob' },
+    { name: 'joe' },
+]
+
+app.get('/users', function(req, res) {
+    res.json({
+        success: true,
+        users: mockUserData,
+        message: 'successfully got users'
+    });
+})
